beca118ee8f1adc53838e0af8a019af3c85c9b0e
lawrence <lawrenceo@activedgetechnologies.com>
2020-07-12 20:07:33 +0100

    customized services

--- src/constants/services.js
+++ src/constants/services.js
@@ -1,22 +1,22 @@
-import React from "react"
-import { FaCode, FaSketch, FaAndroid } from "react-icons/fa"
+import React from "react";
+import { FaCode, FaLaptopCode, FaAndroid } from "react-icons/fa";
 export default [
-  {
-    id: 1,
-    icon: <FaCode className="service-icon" />,
-    title: "web development",
-    text: `I'm baby meditation tofu chillwave, distillery messenger bag thundercats chicharrones kale chips gochujang. Banh mi direct trade marfa salvia.`,
-  },
-  {
-    id: 2,
-    icon: <FaSketch className="service-icon" />,
-    title: "web design",
-    text: `I'm baby meditation tofu chillwave, distillery messenger bag thundercats chicharrones kale chips gochujang. Banh mi direct trade marfa salvia.`,
-  },
-  {
-    id: 3,
-    icon: <FaAndroid className="service-icon" />,
-    title: "app design",
-    text: `I'm baby meditation tofu chillwave, distillery messenger bag thundercats chicharrones kale chips gochujang. Banh mi direct trade marfa salvia.`,
-  },
-]
+	{
+		id    : 1,
+		icon  : <FaLaptopCode className='service-icon' />,
+		title : "Frontend Devlopment",
+		text  : `I'm baby meditation tofu chillwave, distillery messenger bag thundercats chicharrones kale chips gochujang. Banh mi direct trade marfa salvia.`,
+	},
+	{
+		id    : 2,
+		icon  : <FaCode className='service-icon' />,
+		title : "Backend Devlopment",
+		text  : `I'm baby meditation tofu chillwave, distillery messenger bag thundercats chicharrones kale chips gochujang. Banh mi direct trade marfa salvia.`,
+	},
+	{
+		id    : 3,
+		icon  : <FaAndroid className='service-icon' />,
+		title : "Moble App",
+		text  : `I'm baby meditation tofu chillwave, distillery messenger bag thundercats chicharrones kale chips gochujang. Banh mi direct trade marfa salvia.`,
+	},
+];
