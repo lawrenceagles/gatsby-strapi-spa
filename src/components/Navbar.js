6036c62d583309729a55964cf018e57e18ff91dd
lawrence <lawrenceo@activedgetechnologies.com>
2020-07-12 19:53:15 +0100

    made the logo clickable

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,21 +1,24 @@
-import React from "react"
-import logo from "../assets/logo.svg"
-import { FaAlignRight } from "react-icons/fa"
-import PageLinks from "../constants/links"
+import React from "react";
+import { Link } from "gatsby";
+import logo from "../assets/logo.svg";
+import { FaAlignRight } from "react-icons/fa";
+import PageLinks from "../constants/links";
 const Navbar = ({ toggleSidebar }) => {
-  return (
-    <nav className="navbar">
-      <div className="nav-center">
-        <div className="nav-header">
-          <img src={logo} alt="logo" />
-          <button type="button" className="toggle-btn" onClick={toggleSidebar}>
-            <FaAlignRight></FaAlignRight>
-          </button>
-        </div>
-        <PageLinks styleClass="nav-links"></PageLinks>
-      </div>
-    </nav>
-  )
-}
+	return (
+		<nav className='navbar'>
+			<div className='nav-center'>
+				<div className='nav-header'>
+					<Link to='/'>
+						<img src={logo} alt='logo' />
+					</Link>
+					<button type='button' className='toggle-btn' onClick={toggleSidebar}>
+						<FaAlignRight />
+					</button>
+				</div>
+				<PageLinks styleClass='nav-links' />
+			</div>
+		</nav>
+	);
+};
 
-export default Navbar
+export default Navbar;
